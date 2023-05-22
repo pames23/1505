057dd942c0f99a605ddc27d3c7bcb3aea3493f47
pame23 <pameschaparro@gmail.com>
2023-05-22 15:57:56 -0300

    expres listo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log ("hola mundo....")
+const express= require('express')
+const PORT= process.env.PORT || 3001
+const app= express()
+
+
+app.use(express.json())
+app.listen(PORT, ()=>{console.log(`arranco en el puerto ${PORT}`)})
